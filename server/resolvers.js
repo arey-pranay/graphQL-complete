38dc79b77ce1f100f19d797709b3639d30cd4008
Pranay Parikh <pranayparikh02@gmail.com>
2026-02-21 18:41:20 +0530

    Accessing DB, field resolvers, object associations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,14 +1,18 @@
+import { getCompany } from "./db/companies.js";
+import { getJobs } from "./db/jobs.js";
+
 // the resolvers always need to match the schema, so we need to have a resolver for each field in the schema.
 export const resolvers = {
     Query: {
-        job: () => {
-            // when job is queried, this function will be executed and the return value will be sent back to the client as a response.
-            return {
-                id: "1",
-                title: "Software Engineer", // if job { title } is queried, "Software Engineer" will be sent back as a response.
-                description:
-                    "A software engineer is a person who applies the principles of software engineering to the design, development, maintenance, testing, and evaluation of computer software.",
-            };
-        },
+        // when job is queried, this function will be executed and the return value will be sent back to the client as a response.
+        jobs: () => getJobs(),
+    },
+    //the date field needs to be separately resolved because in this case the DB returns "createdAt" in the ISO format, not "date"
+
+    // this is the resolved for all the job objects for the Jobs array, whenever Job is found, the date will be resolved based on this resolver
+    Job: {
+        //even if later we have a date field in the DB, this function will override that response
+        date: (parent) => parent.createdAt.slice(0, "yyyy-mm-dd".length), // we take the "createdAt" field from the parent object (which is the  job object) and slice it to get only the date part (YYYY-MM-DD).
+        company: (parent) => getCompany(parent.companyId), // we take the "companyId" field from the parent object (which is the job object) and use it to get the company details from the database using the getCompanyById function.
     },
 };
