a60bb6948bfa52a0f99988e535a32773eb503d74
Pranay Parikh <pranayparikh02@gmail.com>
2026-02-23 21:35:44 +0530

    created a re-usable fragment and query variable

--- client/src/lib/graphql/queries-mutations.js
+++ client/src/lib/graphql/queries-mutations.js
@@ -22,6 +22,30 @@ const apolloClient = new ApolloClient({
     link: concat(authLink, httpLink),
     cache: new InMemoryCache(),
 });
+
+// fragment is used to avoid writing the same fields repeatedly
+const jobDetailsFragment = gql`
+    fragment JobDetails on Job {
+        id
+        date
+        title
+        company {
+            id
+            name
+        }
+        description
+    }
+`;
+// we can also create our queries as re-usable variables for using the same query in multiple mutations or queries
+const jobByIdQuery = gql`
+    query JobById($id: ID!) {
+        job(id: $id) {
+            ...JobDetails
+        }
+    }
+    ${jobDetailsFragment}
+`;
+
 export async function getJobs() {
     const query = gql`
         # this is the same query we were using in the sandbox at 9000/graphql, but now we are using it in our client side code to fetch the data from the server.
@@ -40,25 +64,15 @@ export async function getJobs() {
 
     // const response = await client.request(query);
     // return response.jobs;
-    const response = await apolloClient.query({ query });
+    const response = await apolloClient.query({
+        query,
+        fetchPolicy: "network-only", // the default is cache-first, which checks in cache first and then puts if not present, and returns if present. but network-only means consider only network requests for this query responses, cache is not supposed to be used for this.
+    });
     return response.data.jobs;
 }
 
 export async function getJob(id) {
-    const query = gql`
-        query ($id: ID!) {
-            job(id: $id) {
-                id
-                title
-                description
-                date
-                company {
-                    id
-                    name
-                }
-            }
-        }
-    `;
+    const query = jobByIdQuery;
     // const response = await client.request(query, { id });
     // return response.job;
     const response = await apolloClient.query({ query, variables: { id } });
@@ -79,8 +93,7 @@ export async function getCompany(id) {
             }
         }
     `;
-    // const response = await client.request(query, { id });
-    // return response.company;
+
     const response = await apolloClient.query({ query, variables: { id } });
     return response.data.company;
 }
@@ -90,15 +103,25 @@ export async function createJob(input) {
     const mutation = gql`
         mutation ($input: CreateJobInput!) {
             createJobMutation(input: $input) {
-                id # this is  the field we will be needing from the mutation response, we need the id of the newly created job to be able to navigate to the job page after creating the job.
+                # these are the fields we will be needing from the mutation response, we need the details of the newly created job to be able to navigate to the job page after creating the job.
+                ...JobDetails
             }
         }
+        ${jobDetailsFragment}
     `;
     // const response = await client.request(mutation, { input });
     // return response.createJobMutation;
     const response = await apolloClient.mutate({
         mutation,
         variables: { input },
+        // after a job is created, we redirect the user to the job's page and then call the query to get job details, It'd be better if we could just store all the job details of this job in cache
+        update: (cache, { data }) => {
+            cache.writeQuery({
+                query: jobByIdQuery,
+                variables: { id: data.createJobMutation.id },
+                data,
+            });
+        },
     });
     return response.data.createJobMutation;
 }
